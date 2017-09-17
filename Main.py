d78ac57e04a5e2a28f3ae9b82f768785bbd430b2
olivierhsta <olivier@hassaoui.com>
2017-09-17 15:10:14 -0400

    All good

--- Main.py
+++ Main.py
@@ -1,26 +1,15 @@
-
-def composition(f1, s1):
-    print("")
-
-f11 = tuple([3, 1, 3, 4])
-f12 = tuple([3, 4, 2, 1])
-s1 = set([f11, f12])
-f1 = tuple([1, 1, 1, 2])
-
-
-composition(f1, s1)
-
-print("ALLO")
-
 def product(p, q):
     return tuple(q[p[i] - 1] for i in range(len(p)))
-    
+
+
 def inverse(p):
     q = [0] * len(p)
+
     for i in range(len(p)):
-        q[p[i-1]] = i + 1
+        q[p[i]-1] = i + 1
     return q
-    
+
+
 def sift(tableau, p):
     IDENTITY = tuple(range(1, len(p)+1))
     q = p
@@ -34,7 +23,8 @@ def sift(tableau, p):
         else:
             q = product(q, inverse(tableau[i][j]))
         return None
-        
+
+
 def appartenance_intelligent(permutations, r):
     IDENTITY = tuple(range(1, len(r)+1))
     tableau = [[IDENTITY] * len(r) for _ in range(len(r))]
@@ -59,8 +49,16 @@ def appartenance_intelligent(permutations, r):
     return sift(tableau, r) is None
     
 # Exemple / Example
-a = tuple([2, 1, 3, 4, 5]) # (12)(3)(4)(5)
-b = tuple([2, 3, 4, 5, 1]) # (12345)
-r = tuple([2, 1, 4, 5, 3]) # (12)(345)
-print(appartenance_intelligent(set([a,b]), r))
+# a = tuple([2, 1, 3, 4, 5]) # (12)(3)(4)(5)
+# b = tuple([2, 3, 4, 5, 1]) # (12345)
+# r = tuple([2, 1, 4, 5, 3]) # (12)(345)
+# print(appartenance_intelligent(set([a, b]), r))
+
+f11 = tuple([3, 1, 3, 4])
+f12 = tuple([3, 4, 2, 1])
+s1 = set([f11, f12])
+f1 = tuple([1, 1, 1, 2])
+
+
+print(appartenance_intelligent(s1, f1))
 
